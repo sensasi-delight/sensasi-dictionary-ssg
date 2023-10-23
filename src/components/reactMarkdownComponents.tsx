fdc613f0000be6fe87fc2cb35bac75db844cbe46
Zain Adam <zainadam.id@gmail.com>
2023-10-23 19:59:23 +0800

    chore: add header anchor

--- src/components/reactMarkdownComponents.tsx
+++ src/components/reactMarkdownComponents.tsx
@@ -8,49 +8,82 @@ import Link from '@mui/material/Link'
 import Typography from '@mui/material/Typography'
 
 import OpenInNewIcon from '@mui/icons-material/OpenInNew'
+import LinkIcon from '@mui/icons-material/Link'
 
 import WordClass from '@/components/WordClass'
 import isWordClass from '@/components/WordClass/isWordClass'
 import isExternalUrl from '@/utils/isExternalUrl'
 
+const HEADER_SX = {
+    '& > svg': {
+        visibility: 'hidden',
+    },
+    '&:hover': {
+        '& > svg': {
+            visibility: 'visible',
+        },
+    },
+}
+
+const HEADER_ANCHOR_STYLE = {
+    textDecoration: 'none',
+    color: 'inherit',
+}
+
+const HEADER_LINK_ICON_STYLE = {
+    verticalAlign: 'middle',
+    marginLeft: '.5rem',
+    color: 'rgba(128, 128, 128, 0.8)',
+}
+
 const reactMarkdownComponents: Partial<Components> = {
-    h1: props => {
-        const newProps = JSON.parse(JSON.stringify(props))
+    h1: ({ children, ...rest }) => {
+        const id = (children as string).toLowerCase().replace(/\s/g, '-')
+
+        const newProps = JSON.parse(JSON.stringify(rest))
         delete newProps.node
 
         return (
             <Typography
-                id={(newProps.children as string)
-                    .toLowerCase()
-                    .replace(/\s/g, '-')}
+                id={id}
                 mt={4}
                 mb={2}
                 variant="h2"
                 component="h1"
-                {...(newProps as TypographyProps)}
-            />
+                sx={HEADER_SX}
+                {...(newProps as TypographyProps)}>
+                <a href={`#${id}`} style={HEADER_ANCHOR_STYLE}>
+                    {children}
+                </a>
+                <LinkIcon style={HEADER_LINK_ICON_STYLE} />
+            </Typography>
         )
     },
 
-    h2: props => {
-        const newProps = JSON.parse(JSON.stringify(props))
-        delete newProps.node
-
-        if (newProps.children === 'Footnotes') {
-            newProps.children = 'Catatan Kaki'
+    h2: ({ children, ...rest }) => {
+        if (children === 'Footnotes') {
+            children = 'Catatan Kaki'
         }
+
+        const id = (children as string).toLowerCase().replace(/\s/g, '-')
+
+        const newProps = JSON.parse(JSON.stringify(rest))
+        delete newProps.node
 
         return (
             <Typography
-                id={(newProps.children as string)
-                    .toLowerCase()
-                    .replace(/\s/g, '-')}
+                id={id}
                 mt={4}
                 mb={2}
                 variant="h5"
                 component="h2"
-                {...(newProps as TypographyProps)}
-            />
+                sx={HEADER_SX}
+                {...(newProps as TypographyProps)}>
+                <a href={`#${id}`} style={HEADER_ANCHOR_STYLE}>
+                    {children}
+                </a>
+                <LinkIcon style={HEADER_LINK_ICON_STYLE} />
+            </Typography>
         )
     },
 
